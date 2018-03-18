39ef8c0c81b644194d70eeca328e64f7cee47c75
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-18 19:41:07 +0000

    finalized spec and main file

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,8 +1,14 @@
 class Roman
-    ROMAN = {10 => "X", 5 => "V", 1 => "I"} 
+    ROMAN = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 
+    90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 
+    4 => "IV", 1 => "I"} 
     
     def self.convert(number)
-        ROMAN[number] 
+        roman = ""
+        ROMAN.each do |k, v|
+            (number / k).times { roman << v; number = number -= k }
+        end
+        roman
     end    
         
 end    
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -11,5 +11,17 @@ describe Roman do
     
     it "should be able to convert 10 to X" do
         expect(Roman.convert(10)).to eq("X")
+    end
+    
+    it "should be able to convert 35 to XXXV" do
+        expect(Roman.convert(35)).to eq ("XXXV")
+    end
+    
+    it "should be able to convert 996 to CMXCVI" do
+        expect(Roman.convert(996)).to eq ("CMXCVI")
+    end    
+    
+    it "should be able to convert 1996 to MCMXCVI" do
+        expect(Roman.convert(1996)).to eq "MCMXCVI"
     end    
 end
